e3e46f577b2eb1abb8ffb7025d6c98e4e3f28018
Pratyush <146558849+FLACK277@users.noreply.github.com>
2025-07-11 19:05:24 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,4 +35,4 @@ app.include_router(skillassess.router)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # default to 8000 if PORT is not set
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
