b7dfbd26815516417f4865added9c9d11ffb93bc
Pratyush <pratyushshankar18@gmail.com>
2025-07-12 23:25:49 +0530

    fixed nixpacks command and main.py uvicorn command

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,4 +42,4 @@ app.include_router(skillassess.router)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # default to 8000 if PORT is not set
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
